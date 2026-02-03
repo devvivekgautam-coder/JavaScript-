c7f90089e54df3dc218d0c473d3f214ebf29af94
devvivekgautam-coder <dev.vivekgautam@gmail.com>
2026-02-03 15:16:18 +0530

    some programme of recusion

---
+++ 6th-Lecture-Recursion/fibonacci.js
@@ -0,0 +1,13 @@
+function fibo(n) {
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
+    else {
+        return fibo(n - 1) + fibo(n - 2)
+    }
+}
+
+console.log(fibo(8));
