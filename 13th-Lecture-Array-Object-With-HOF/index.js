49c699711187d3e831650b06d2f9b96727830731
devvivekgautam-coder <dev.vivekgautam@gmail.com>
2026-02-20 16:40:37 +0530

    array object with HOF

---
+++ 13th-Lecture-Array-Object-With-HOF/index.js
@@ -0,0 +1,52 @@
+let arr = [
+    {
+        empId: 1,
+        name: "Shrey",
+        department: "Education",
+        salary: 10000
+    },
+    {
+        empId: 2,
+        name: "Jay",
+        department: "Business",
+        salary: 13000
+    },
+    {
+        empId: 3,
+        name: "Janvi",
+        department: "Business",
+        salary: 14000
+    },
+    {
+        empId: 4,
+        name: "Vaishu",
+        department: "Job",
+        salary: 12000
+    },
+    {
+        empId: 5,
+        name: "Anant G",
+        department: "Education",
+        salary: 14000
+    }
+];
+
+// let shortArr = arr.sort((x, y) => {
+//     return (y.department === "Business") - (x.department === "Business");
+// });
+
+            // OR
+
+// let filterArr = arr.filter((x) => {
+//     return  x.department == "Business";
+// });
+
+// let findArr = arr.find((n) => {
+//     return n.name == "Shrey"
+// })
+
+// let mapArr = arr.map((m) => {
+//     return m.salary = m.salary + m.salary * 10 / 100;
+// })
+
+// console.log(arr);
