ef57c4d25594a49d7e0a55bea09911f2146cc0d5
devvivekgautam-coder <dev.vivekgautam@gmail.com>
2026-04-12 22:25:20 +0530

    add hover

--- Lecture-API/project-api/assets/js/script.js
+++ Lecture-API/project-api/assets/js/script.js
@@ -1,20 +1,77 @@
 async function myAPI() {
-    let isResponse = await fetch("https://dog.ceo/api/breeds/list/all");
-    let data = await isResponse.json();
-    return data;
+    let res = await fetch("https://dog.ceo/api/breeds/list/all");
+    return await res.json();
 }
+
+async function getBreedImages(breed) {
+    let res = await fetch(`https://dog.ceo/api/breed/${breed}/images`);
+    return await res.json();
+}
+
+async function getSubBreedImages(breed, subBreed) {
+    let res = await fetch(`https://dog.ceo/api/breed/${breed}/${subBreed}/images`);
+    return await res.json();
+}2
 
 async function run() {
     let breedList = document.querySelector(".breedList");
+    let rightSide = document.querySelector(".rightSight");
 
     try {
         let res = await myAPI();
-        console.log(res);
-
         let breeds = res.message;
 
         for (let breed in breeds) {
-            breedList.innerHTML += `<li class="mb-2 bg-info list-unstyled">${breed}</li>`;
+
+            let p = document.createElement("p");
+            p.innerText = breed;
+
+            p.addEventListener("click", async () => {
+
+                document.querySelectorAll(".breedList p").forEach(el => {
+                    el.classList.remove("active");
+                });
+                p.classList.add("active");
+
+                rightSide.innerHTML = "<h4>Loading...</h4>";
+
+                let subBreeds = breeds[breed];
+                rightSide.innerHTML = "";
+
+                if (subBreeds.length > 0) {
+
+                    for (let sub of subBreeds) {
+
+                        let subTitle = document.createElement("h3");
+                        subTitle.innerText = `${breed} - ${sub}`;
+                        rightSide.appendChild(subTitle);
+
+                        let imgData = await getSubBreedImages(breed, sub);
+
+                        imgData.message.slice(0,).forEach(img => {
+                            let image = document.createElement("img");
+                            image.src = img;
+                            rightSide.appendChild(image);
+                        });
+                    }
+
+                } else {
+
+                    let title = document.createElement("h2");
+                    title.innerText = breed;
+                    rightSide.appendChild(title);
+
+                    let imgData = await getBreedImages(breed);
+
+                    imgData.message.slice(0,).forEach(img => {
+                        let image = document.createElement("img");
+                        image.src = img;
+                        rightSide.appendChild(image);
+                    });
+                }
+            });
+
+            breedList.appendChild(p);
         }
 
     } catch (err) {
